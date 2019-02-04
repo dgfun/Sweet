524c91dcb180fd39c19b218f059b53a791fa67f7
dgfun <46782895+dgfun@users.noreply.github.com>
2019-02-04 04:23:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>奇趣保罗</title>
+    <title>捣鼓之乐</title>
     <link href="static/32.png" rel="icon" sizes="32x32"/>
     <link href="static/192.png" rel="icon" sizes="192x192"/>
     <link href="static/paul.css" rel="stylesheet" type="text/css"/>
@@ -19,15 +19,12 @@
     <div class="content">
         <figure class="me">
             <img class="avatar" src="./static/avatar.jpg"/>
-            <h2 class="name">奇趣保罗</h2>
+            <h2 class="name">捣鼓之乐</h2>
         </figure>
         <section id="main" class="active">
-            <a href="https://paugram.com"><i class="fa fa-home"></i><span class="title">博客</span></a>
-            <a href="http://weibo.com/234891753/" target="_blank"><i class="fa fa-weibo"></i><span class="title">微博</span></a>
-            <a href="http://music.163.com/#/user/home?id=7041859" target="_blank"><i class="fa fa-music"></i><span class="title">网易云</span></a>
-            <a href="http://steamcommunity.com/id/dreamer-paul/" target="_blank"><i class="fa fa-steam"></i><span class="title">Steam</span></a>
-            <a href="https://github.com/dreamer-paul/" target="_blank"><i class="fa fa-github"></i><span class="title">GitHub</span></a>
-            <a href="https://space.bilibili.com/124512959/" target="_blank"><i class="fa fa-flag"></i><span class="title">哔哩哔哩</span></a>
+            <a href="https://daogu.fun"><i class="fa fa-home"></i><span class="title">博客</span></a>
+            <a href="https://pan.daogu.fun" target="_blank"><i class="fa fa-home"></i><span class="title">云盘</span></a>
+            <a href="https://pay.daogu.fun" target="_blank"><i class="fa fa-flag"></i><span class="title">商店</span></a>
         </section>
         <section id="articles">
 <?php
@@ -62,12 +59,9 @@ else{
             <a class="item" href="https://kico.binkic.com/style" target="_blank">Kico Style<span class="meta">极简前端框架</span></a>
             <a class="item" href="https://kico.binkic.com/player" target="_blank">Kico Player<span class="meta">极简音乐播放器</span></a>
             <a class="item" href="javascript:alert('没做好啊，来打我啊~');">Kico Tools<span class="meta">在线时钟小程序</span></a>
-            <a class="item" href="https://github.com/Dreamer-Paul/Single" target="_blank">Single<span class="meta">以单身为灵感的主题</span></a>
-            <a class="item" href="https://www.binkic.com" target="_blank">缤奇官网<span class="meta">采用 TC 开发的团队官网</span></a>
-            <a class="item" href="https://dreamer-paul.github.io/Meeting" target="_blank">梦想主题晨会<span class="meta">以梦想为主题的班级晨会</span></a>
         </section>
         <section id="about">
-            <p>我是一个热爱钻研计算机技术的青少年，从小开始对计算机有极大的兴趣。目前我专注网站开发领域。我还自己发起建立了一个制作游戏的团队，希望能和大家合作，一同创造出属于我们自己的原创作品。<a href="https://paugram.com/about.html" target="_blank">详细阅读</a></p>
+            <p>待加工<a href="https://daogu.fun/about.html" target="_blank">详细阅读</a></p>
         </section>
         <div class="actions">
             <div class="item home active">
@@ -90,7 +84,7 @@ else{
     </div>
 </main>
 <footer>
-    <p>© <?php echo date(Y)?> By <a href="https://paugram.com">Dreamer-Paul</a>.</p>
+    <p>About <?php echo date(Y)?> <a href="https://daogu.fun">Amos</a>.</p>
 </footer>
 
 <script src="static/paul.js"></script>
